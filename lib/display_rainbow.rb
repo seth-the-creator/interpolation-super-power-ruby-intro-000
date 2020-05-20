96f04c3b651292bef8c77d8ec7bd36a0f82fa03b
Seth Mueller <sethmueller1@gmail.com>
2020-05-20 06:10:38 +0000

    Automatically backed up by Learn

--- lib/display_rainbow.rb
+++ lib/display_rainbow.rb
@@ -3,7 +3,6 @@ def display_rainbow(anything)
   puts anything
 end
 
-colors = ["R: red", "O: orange", "Y: yellow", "G: green", "B: blue", "I: in
-digo", "V: violet"]
+# colors = ["R: red", "O: orange", "Y: yellow", "G: green", "B: blue", "I: indigo", "V: violet"]
 
 # display_rainbow(colors)
